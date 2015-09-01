4130b564c3e70cdba431cfcb650e3c037db8a45f
Patrick Lemiuex <plemiuex@rubiconproject.com>
2015-08-31 17:53:50 -0700

    added word net files

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: "reduxfj:meteor-words",
+  summary: "A Meteor Wrapper for wordnet",
+  version: "0.9",
+  git: "https://github.com/reduxdj/meteor-words"
+});
+
+Npm.depends({
+    "WNdb": "3.0.1"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('wordnet.js', 'server');
+});
