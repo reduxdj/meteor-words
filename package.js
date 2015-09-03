01f5cf00fb878b52ccc8cfee00ea9a9d4ea054f6
Patrick Lemiuex <plemiuex@rubiconproject.com>
2015-09-03 00:46:22 -0700

    package fix

--- package.js
+++ package.js
@@ -5,13 +5,13 @@ Package.describe({
   git: "https://github.com/reduxdj/meteor-words"
 });
 
+Package.on_use(function (api, where) {
+  api.add_files('wordnet.js', 'server')
+  api.export('WordNet')
+  api.export('wndb')
+});
+
 Npm.depends({
   "node-wordnet" : "0.1.11",
   "wndb-with-exceptions" : "3.0.1"
-});
-
-Package.on_use(function (api, where) {
-  api.add_files('wordnet.js', 'server')
-  api.export('WordNet','server')
-  api.export('wndb','server')
 });
